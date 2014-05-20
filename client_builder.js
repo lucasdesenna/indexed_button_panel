1dc68227bb74a86fd538d6cf24d7fc7190b2b50e
lucasdesenna <lucasdesenna@gmail.com>
2014-05-20 18:17:54 -0300

    Revert "Db Local [quebrado]"
    This reverts commit 4782353c7a863c7e15dc364c9e65453f2d5456a2.

--- client_builder.js
+++
@@ -1,7 +0,0 @@
-function createIndexedButtonPanel() {
-	localDb.transaction(function (tr) {
-		tr.executeSql("SELECT DISTINCT search_attr FROM keywords", [], function(tr, results){
-			console.log(results);
-		});
-    });
-}
---
+++ dummy_data.js
@@ -0,0 +1,5 @@
+var dependency = {
+    "earache" = [ "excited","good-bye", "grandma", "grocery", "indoors", "January", "kitchen", "lullaby", "monster", "morning", "naughty", "October", "pajamas", "pretend", "quarter", "shampoo", "stomach", "there's", "they'll", "they're", "they've"],
+    "tonight" = [ "unhappy", "airplane", "alphabet", "bathroom", "favorite", "medicine", "December", "dinosaur", "elephant", "February", "football"],
+    "forehead" = [ "headache", "hospital", "lollipop", "November", "outdoors", "question", "railroad", "remember"]
+}
